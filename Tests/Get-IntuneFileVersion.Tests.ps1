102571a03a0968ef84083423f843e4cb1f75bbba
Gioxx <giovanni@solone.it>
2026-07-17 09:26:09 +0200

    test: fix Pester 5 compatibility

--- Tests/Get-IntuneFileVersion.Tests.ps1
+++ Tests/Get-IntuneFileVersion.Tests.ps1
@@ -1,9 +1,11 @@
-. (Join-Path $PSScriptRoot '..\Public\Get-IntuneFileVersion.ps1')
-
 Describe 'Get-IntuneFileVersion' {
+    BeforeAll {
+        . (Join-Path $PSScriptRoot '..\Public\Get-IntuneFileVersion.ps1')
+    }
+
     It 'returns the FileVersion for an executable' {
         $path = Join-Path $PSHOME 'pwsh.exe'
-        Get-IntuneFileVersion -Path $path | Should Be ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($path).FileVersion)
+        Get-IntuneFileVersion -Path $path | Should -Be ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($path).FileVersion)
     }
 
     It 'throws when the path does not exist' {
@@ -14,7 +16,7 @@ Describe 'Get-IntuneFileVersion' {
         } catch {
             $threw = $true
         }
-        $threw | Should Be $true
+        $threw | Should -Be $true
     }
 
     It 'throws when the path is a directory' {
@@ -24,6 +26,6 @@ Describe 'Get-IntuneFileVersion' {
         } catch {
             $threw = $true
         }
-        $threw | Should Be $true
+        $threw | Should -Be $true
     }
 }
--- Tests/Module.Tests.ps1
+++ Tests/Module.Tests.ps1
@@ -1,12 +1,15 @@
 Describe 'IntuneWinAppUtilGUI module' {
+    BeforeAll {
+        $manifestPath = Join-Path $PSScriptRoot '..\IntuneWinAppUtilGUI.psd1'
+        $module = Import-Module $manifestPath -Force -PassThru
+    }
+
     It 'has a valid manifest and exports the file version helper' {
-        $manifest = Test-ModuleManifest -Path (Join-Path $PSScriptRoot '..\IntuneWinAppUtilGUI.psd1')
-        $manifest.Version.ToString() | Should Be '1.0.9'
-        (@($manifest.ExportedFunctions.Keys) -contains 'Get-IntuneFileVersion') | Should Be $true
+        $module.Version.ToString() | Should -Be '1.0.9'
+        @($module.ExportedFunctions.Keys) | Should -Contain 'Get-IntuneFileVersion'
     }
 
     It 'imports the file version helper' {
-        Import-Module (Join-Path $PSScriptRoot '..\IntuneWinAppUtilGUI.psd1') -Force
-        (Get-Command Get-IntuneFileVersion -ErrorAction Stop).Source | Should Be 'IntuneWinAppUtilGUI'
+        (Get-Command Get-IntuneFileVersion -ErrorAction Stop).Source | Should -Be 'IntuneWinAppUtilGUI'
     }
 }
